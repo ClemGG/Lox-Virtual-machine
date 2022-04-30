bcd095eb0b3dc3668d420dbf312256107e047b08
Clement Guichard <c.guichard508@laposte.net>
2022-04-30 20:18:48 +0200

    added bytecode instructions & constants

--- Core/chunk.h
+++ Core/chunk.h
@@ -1,8 +1,10 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+ OP_CONSTANT,
  OP_RETURN,
 } OpCode;
 
@@ -10,9 +12,12 @@ typedef struct {
  int count;
  int capacity;
  uint8_t* code;
+ int* lines;
+ ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 #endif
