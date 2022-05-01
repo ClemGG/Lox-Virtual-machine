da91a6716a7f4b2ad5ef4b5aa1a975bafc508e6f
Clement Guichard <c.guichard508@laposte.net>
2022-05-01 19:01:08 +0200

    added string type & memory cleanup on exit

--- Core/compiler.h
+++ Core/compiler.h
@@ -1,6 +1,7 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 #include "vm.h"
+#include "object.h"
 
 bool compile(const char* source, Chunk* chunk);
 
--- Core/value.c
+++ Core/value.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -31,6 +33,7 @@ void printValue(Value value) {
          break;
      case VAL_NIL: printf("nil"); break;
      case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+     case VAL_OBJ: printObject(value); break;
  }
 }
 
@@ -40,7 +43,12 @@ bool valuesEqual(Value a, Value b) {
  case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
  case VAL_NIL: return true;
  case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+ case VAL_OBJ: {
+         ObjString* aString = AS_STRING(a);
+         ObjString* bString = AS_STRING(b);
+         return aString->length == bString->length && memcmp(aString->chars, bString->chars, aString->length) == 0;
+     }
  default:
- return false; // Unreachable.
+    return false; // Unreachable.
  }
 }
