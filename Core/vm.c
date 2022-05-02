117fd4bb6bee82c5d587d50f5cfaf20da7645ada
Clement Guichard <c.guichard508@laposte.net>
2022-05-02 14:26:36 +0200

    added hash table for variable hookup

--- Core/vm.c
+++ Core/vm.c
@@ -30,9 +30,12 @@ static void runtimeError(const char* format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.strings);
+
 }
 
 void freeVM() {
+    freeTable(&vm.strings);
     freeObjects();
 }
 
--- Core/vm.h
+++ Core/vm.h
@@ -1,6 +1,7 @@
 #ifndef clox_vm_h
 #define clox_vm_h
 #include "chunk.h"
+#include "table.h"
 #include "value.h"
 
 
@@ -11,6 +12,7 @@ typedef struct {
  uint8_t* ip;
  Value stack[STACK_MAX];
  Value* stackTop;
+ Table strings;
  Obj* objects;
 } VM;
 
