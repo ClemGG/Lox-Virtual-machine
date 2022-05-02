68ba49d85854b70ea44fcbada4c07515e8eebf5a
Clement Guichard <c.guichard508@laposte.net>
2022-05-02 16:32:14 +0200

    added for statement

--- Core/compiler.c
+++ Core/compiler.c
@@ -414,12 +414,14 @@ static void defineVariable(uint8_t global) {
  }
  emitBytes(OP_DEFINE_GLOBAL, global);
 }
+
 static void and_(bool canAssign) {
  int endJump = emitJump(OP_JUMP_IF_FALSE);
  emitByte(OP_POP);
  parsePrecedence(PREC_AND);
  patchJump(endJump);
 }
+
 static ParseRule* getRule(TokenType type) {
  return &rules[type];
 }
@@ -489,6 +491,43 @@ static void expressionStatement() {
  consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
  emitByte(OP_POP);
 }
+static void forStatement() {
+ beginScope();
+ consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+ if (match(TOKEN_SEMICOLON)) {
+ // No initializer.
+ } else if (match(TOKEN_VAR)) {
+ varDeclaration();
+ } else {
+ expressionStatement();
+ }
+ int loopStart = currentChunk()->count;
+ int exitJump = -1;
+ if (!match(TOKEN_SEMICOLON)) {
+ expression();
+ consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+ // Jump out of the loop if the condition is false.
+ exitJump = emitJump(OP_JUMP_IF_FALSE);
+ emitByte(OP_POP); // Condition.
+ }
+ if (!match(TOKEN_RIGHT_PAREN)) {
+ int bodyJump = emitJump(OP_JUMP);
+ int incrementStart = currentChunk()->count;
+ expression();
+ emitByte(OP_POP);
+ consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+ emitLoop(loopStart);
+ loopStart = incrementStart;
+ patchJump(bodyJump);
+ }
+ statement();
+ emitLoop(loopStart);
+ if (exitJump != -1) {
+ patchJump(exitJump);
+ emitByte(OP_POP); // Condition.
+ }
+ endScope();
+}
 static void ifStatement() {
  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
  expression();
@@ -516,6 +555,9 @@ static void statement() {
  }
  else if (match(TOKEN_IF)) {
  ifStatement();
+ }
+ else if (match(TOKEN_FOR)) {
+ forStatement();
  }
  else if (match(TOKEN_WHILE)) {
  whileStatement();
