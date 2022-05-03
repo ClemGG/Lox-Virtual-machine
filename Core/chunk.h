66faa765b8eb55aa5596527ee54ec2b85a054ebe
Clement Guichard <c.guichard508@laposte.net>
2022-05-03 16:56:19 +0200

    added classes and instances

--- Core/chunk.h
+++ Core/chunk.h
@@ -16,6 +16,8 @@ typedef enum {
  OP_SET_GLOBAL,
  OP_GET_UPVALUE,
  OP_SET_UPVALUE,
+ OP_GET_PROPERTY,
+ OP_SET_PROPERTY,
  OP_EQUAL,
  OP_GREATER,
  OP_LESS,
@@ -33,6 +35,7 @@ typedef enum {
  OP_CLOSURE,
  OP_CLOSE_UPVALUE,
  OP_RETURN,
+ OP_CLASS,
 } OpCode;
 
 typedef struct {
